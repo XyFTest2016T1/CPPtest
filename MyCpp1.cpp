847d6f916b3f3f877e81088d780869b0f978ba0a
Nino <Nino.Zuljevic@pnnl.gov>
2017-04-07 16:17:26 -0700

    Improvements for making the scoring work

--- MyCpp1.cpp
+++ MyCpp1.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 #include<fstream>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::ofstream outFile;
-	outFile.open("solution1.txt");
-	outFile <<  "sample solution1\n";
+	int ret_code = 0;
+
+	std::ifstream inFile((std::string(argv[1]) + std::string("/solution1.txt")).c_str(), std::ios::binary);
+	std::ofstream outFile("solution1.txt", std::ios::binary);
+
+	if(inFile && outFile)
+		outFile << inFile.rdbuf();
+	else
+	{
+		std::cerr << "Error reading/writing specified files" << std::endl;
+		ret_code = 1;
+	}
+
 	outFile.close();
-	return 0;
+	inFile.close();
+
+	return ret_code;
+
 }
